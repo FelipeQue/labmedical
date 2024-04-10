6adbf5e566cc44fae7d4b40824dd117fe5b0c220
Felipe Quérette <felipe.querette@gmail.com>
2024-04-10 00:44:41 -0300

    created signup modal and its form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, inject, ViewEncapsulation, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class LoginComponent {
+
+  @ViewChild('signupSuccess') signupSuccess!: TemplateRef<any>;
+  private modalService = inject(NgbModal);
+  private modalRef: any;
+
+  loginInfo = new FormGroup({
+    userEmail: new FormControl(''),
+    userPassword: new FormControl(''),
+  });
+
+  signupInfo = new FormGroup({
+    userEmail: new FormControl('', [Validators.required, Validators.email]),
+    userPassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
+  });
+
+  login() {
+    console.log("Botão de login foi clicado.");
+  };
+
+  openModal(content: TemplateRef<any>) {
+    this.modalRef = this.modalService.open(content, { centered: true });
+  };
+
+  closeModal(reason: string) {
+    this.modalRef.dismiss(reason);
+  };
+
+  signup() {
+    this.signupInfo.reset();
+    this.closeModal("Submit click");
+    this.openModal(this.signupSuccess); 
+  };
+
+
 
 }
