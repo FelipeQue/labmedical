757712cd581dad9b937c035f87e4181e436c42ef
Felipe Quérette <felipe.querette@gmail.com>
2024-04-10 20:16:44 -0300

    created toolbar and sidemenu components

---
+++ src/app/components/sidemenu/sidemenu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidemenu',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidemenu.component.html',
+  styleUrl: './sidemenu.component.scss'
+})
+export class SidemenuComponent {
+
+}
