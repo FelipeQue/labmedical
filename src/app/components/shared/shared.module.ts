f7cecaf9ce48818bda2e31dc37413fd0ad82456f
Felipe Quérette <felipe.querette@gmail.com>
2024-04-11 18:45:39 -0300

    added icons to the sidemenu

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [ToolbarComponent,SidemenuComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   exports: [ToolbarComponent,SidemenuComponent],
 })
 export class SharedModule { }
