d442a88fca9cf38ef70019961a5937bd15190ae7
Felipe Quérette <felipe.querette@gmail.com>
2024-04-11 00:11:26 -0300

    logout implemented

--- src/app/components/shared/sidemenu/sidemenu.component.ts
+++ src/app/components/shared/sidemenu/sidemenu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidemenu',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './sidemenu.component.scss'
 })
 export class SidemenuComponent {
+
+  constructor (private router: Router) {};
+
+  logout(){
+    localStorage.setItem("loggedUser", JSON.stringify(""));
+    this.router.navigate(["login"]);
+    alert("Logout realizado com sucesso.");
+  };
+
 
 }
