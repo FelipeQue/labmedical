6b997fb1cab2de2235212102d5a7c483177b8034
Felipe Quérette <felipe.querette@gmail.com>
2024-04-11 17:40:24 -0300

    added avatar and dynamic title to toolbar

--- src/app/components/shared/toolbar/toolbar.component.ts
+++ src/app/components/shared/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-toolbar',
@@ -10,12 +12,20 @@ export class ToolbarComponent {
   loggedUserName: string = "";
   loggedUser: any;
 
-  constructor() {
+  activeRoute: string = "";
+
+  constructor(private router: Router) {
     const loggedUserStorage = localStorage.getItem("loggedUser");
     if (loggedUserStorage) {
       this.loggedUser = JSON.parse(loggedUserStorage);
       this.loggedUserName = this.loggedUser.name;
     };
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd && event.url) {
+        this.activeRoute = event.url;
+      }
+    });
   };
 
 
