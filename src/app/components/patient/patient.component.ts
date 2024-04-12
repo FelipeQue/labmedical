3014b0048db001429ea55b8aeccc67f7762d8f9f
Felipe Quérette <felipe.querette@gmail.com>
2024-04-11 21:52:12 -0300

    started creating patient page form

--- src/app/components/patient/patient.component.ts
+++ src/app/components/patient/patient.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-patient',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './patient.component.html',
   styleUrl: './patient.component.scss'
 })
 export class PatientComponent {
+
+  patientInfo = new FormGroup({
+    patientName: new FormControl('', Validators.required),
+
+    userPassword: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(4)]),
+  });
+
+
 
 }
