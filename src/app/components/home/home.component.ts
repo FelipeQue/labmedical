b2ce3d6f88e81c3e6215a9972e9691ea011724f8
Felipe Quérette <felipe.querette@gmail.com>
2024-04-17 23:26:43 -0300

    added system stats to the home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,25 +1,70 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { PatientService } from '../../services/patient.service';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { BirthDatePipe } from '../../pipes/birth-date.pipe';
+import { ToastrService } from 'ngx-toastr';
+import { ConsultationService } from '../../services/consultation.service';
+import { ExamService } from '../../services/exam.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, BirthDatePipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
-  patients: any | undefined;
-
-  constructor (private patientService: PatientService) {
-
-    this.patientService.getPatient().subscribe((patients) => {this.patients = patients});
-
+  constructor (
+    private patientService: PatientService,
+    private toastrService: ToastrService,
+    private examService: ExamService,
+    private consultationService: ConsultationService,
+  ) {
   }
 
+  patientInput = new FormGroup({
+    nameOrId: new FormControl('')
+  });
 
+  resultsList: any = [];
+  patientsList: any = [];
+
+  patientsAmount: number = 0;
+  examsAmount: number = 0;
+  consultationsAmount: number = 0;
+
+  ngOnInit() {
+    this.patientService.getPatient().subscribe((patients) => {
+      this.patientsList = patients;
+      this.patientsAmount = this.patientsList.length;
+    });
+    this.examService.getExam().subscribe((exams) => {
+      let examsArray = exams;
+      this.examsAmount = examsArray.length;
+    });
+    this.consultationService.getConsultation().subscribe((consultations) => {
+      let consultationsArray = consultations;
+      this.consultationsAmount = consultationsArray.length;
+    });
+  }
+
+  searchPatient() {
+    const nameOrId = this.patientInput.value.nameOrId?.trim();
+    if (nameOrId) {
+      this.patientService.getPatient().subscribe((patients) => {
+        this.patientsList = patients;
+        this.resultsList = this.patientsList.filter((searchedPatient: { name: string, id: string }) => {
+          const isNameMatch = searchedPatient.name && searchedPatient.name.toLowerCase().includes(nameOrId.toLowerCase());
+          const isIdMatch = searchedPatient.id && searchedPatient.id.includes(nameOrId);
+          return isNameMatch || isIdMatch;
+        });
+      });
+    } else {
+      this.toastrService.warning("Preencha nome ou identificador no campo de busca.");
+    }
+  };
 
 
 
