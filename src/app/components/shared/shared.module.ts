f42078f781b020379cccf3a7b711a34088380567
Felipe Quérette <felipe.querette@gmail.com>
2024-04-21 01:05:00 -0300

    added feature: logout from dropdown on toolbar

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { SidemenuComponent } from './sidemenu/sidemenu.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterLink } from '@angular/router';
 import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [ToolbarComponent,SidemenuComponent, ConfirmDialogComponent],
-  imports: [CommonModule, FontAwesomeModule, RouterLink],
+  imports: [CommonModule, FontAwesomeModule, RouterLink, NgbDropdownModule],
   exports: [ToolbarComponent,SidemenuComponent, ConfirmDialogComponent],
 })
 export class SharedModule { }
